c0c3a9b367350065e658b7b74215fbc347412225
Irvene Kwambana <irvenehawks@gmail.com>
2022-09-28 21:35:45 +0200

    Project setup

---
+++ backend/src/test/java/net/irvinehawks/springBoot/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.irvinehawks.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Employee management system</h1>
+    </div>
+  );
+}
+
+export default App;
